eaa363cf1d0e259de089c204a9170a6b698b5c05
wander <wanderdesigner@gmail.com>
2023-05-14 11:47:24 -0300

    initial commit

---
+++ script01.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# PARAMETROS
+# $0 - nome do programa
+# $# - quantidade de parametros
+# $* - todos os parametros inseridos
+# $1-9 - cada parametro informado
+
+clear
+read -p "Digite o seu nome: " USER
+RETURN_CODE=$?
+clear
+echo "============ START ================================="
+echo ""
+date
+echo "processo:" $$
+echo "ola $USER"
+echo "return code da leitura da variavel: $RETURN_CODE"
+echo ""
+echo "===================================================="
+echo ""
+echo "o script $0 recebeu $# parametros"
+
+if [ $# -eq 0 ]
+then
+	echo "voce nao informou numeros para a somar"
+else
+	echo "total da soma de parametros: $(expr $1 + $2)"
+fi
+
+echo ""
+echo "============ END ==================================="
+
+exit 0
